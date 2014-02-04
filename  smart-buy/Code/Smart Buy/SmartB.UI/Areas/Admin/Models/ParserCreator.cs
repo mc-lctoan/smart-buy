d757be3c06994ed87bc6eaa5fa74bab5ebfcbfa8
doanhoanhtriet@gmail.com <doanhoanhtriet@gmail.com@71cf5126-8dba-bb00-75b8-2b74b3ec1fb7>
2014-02-04 17:09:10 +0000

    - Add property to model. - More items in UI. - Switch xpath value receiver: done.

---  smart-buy/Code/Smart Buy/SmartB.UI/Areas/Admin/Models/ParserCreator.cs
+++  smart-buy/Code/Smart Buy/SmartB.UI/Areas/Admin/Models/ParserCreator.cs
@@ -10,5 +10,11 @@ namespace SmartB.UI.Areas.Admin.Models
     {
         [Display(Name = "Link cần lấy")]
         public string ParseLink { get; set; }
+
+        [Display(Name = "Tên sản phẩm")]
+        public string ProductNameXpath { get; set; }
+
+        [Display(Name = "Giá sản phẩm")]
+        public string PriceXpath { get; set; }
     }
 }
