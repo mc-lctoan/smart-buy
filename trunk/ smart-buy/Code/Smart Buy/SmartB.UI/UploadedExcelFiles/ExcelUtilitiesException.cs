f2e2cec5d6d19a91ecabc15b37e03c8f22c33498
Acam37@gmail.com <Acam37@gmail.com@71cf5126-8dba-bb00-75b8-2b74b3ec1fb7>
2014-02-12 07:35:28 +0000

    Commit file ExcelUtilitiesException.cs

---
+++ trunk/ smart-buy/Code/Smart Buy/SmartB.UI/UploadedExcelFiles/ExcelUtilitiesException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartB.UI.UploadedExcelFiles
+{
+    [Serializable()]
+    public class InvalidNumberException : Exception
+    {
+        public InvalidNumberException()
+            : base()
+        {
+        }
+        public InvalidNumberException(string message)
+            : base(message) { }
+
+        public InvalidNumberException(int value)
+        {
+        }
+        public InvalidNumberException(string message, Exception e) : base(message, e) { }
+
+    }
+}
