dc133a74358a484a028e1387f31df8612cf8a462
Acam37@gmail.com <Acam37@gmail.com@71cf5126-8dba-bb00-75b8-2b74b3ec1fb7>
2014-02-13 16:33:47 +0000

    Valid Import Excel : + Chia Bảng + valid errorList, valid data input

---  smart-buy/Code/Smart Buy/SmartB.UI/Models/SellProductModels.cs
+++  smart-buy/Code/Smart Buy/SmartB.UI/Models/SellProductModels.cs
@@ -9,9 +9,9 @@ namespace SmartB.UI.Models
     public class SellProductModel
     {
          public int Id { get; set; }
-        [MaxLength(20)]
+        [MaxLength(100)]
         [Required(ErrorMessage = "Vui lòng nhập tên sản phẩm")]
-        [StringLength(20, ErrorMessage = "Tên sản phẩm phải từ 5 đến 20 ký tự."
+        [StringLength(100, ErrorMessage = "Tên sản phẩm phải từ 5 đến 20 ký tự."
             , MinimumLength = 5)]
         public string Name { get; set; }
         [MaxLength(20)]
@@ -22,6 +22,7 @@ namespace SmartB.UI.Models
         [Required(ErrorMessage = "Vui lòng nhập giá sản phẩm")]
         [Range(0, 9999999, ErrorMessage = "Vui lòng nhập giá trị từ 1 đến 10000")]
         [DisplayFormat(DataFormatString = "{0:0,000}")]
-        public int Price { get; set; } 
+        public int Price { get; set; }
+        public int RowNumber { get; set; }
     }
 }
