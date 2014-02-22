84a30bf0a2717b1b94c69aeea05082df856d0269
doanhoanhtriet@gmail.com <doanhoanhtriet@gmail.com@71cf5126-8dba-bb00-75b8-2b74b3ec1fb7>
2014-02-22 16:51:40 +0000

    Define route: done.

---
+++ trunk/ smart-buy/Code/Smart Buy/SmartB.UI/Controllers/RouteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartB.UI.Controllers
+{
+    public class RouteController : Controller
+    {
+        //
+        // GET: /Route/
+
+        public ActionResult DefineRoute()
+        {
+            return View();
+        }
+
+    }
+}
