52832aec0789c769ca8a7df5adfd3c8159f249ff
doanhoanhtriet@gmail.com <doanhoanhtriet@gmail.com@71cf5126-8dba-bb00-75b8-2b74b3ec1fb7>
2014-02-23 16:02:14 +0000

    - Add RouteBoxer.js - Detect nearby markets, done.

---  smart-buy/Code/Smart Buy/SmartB.UI/Controllers/RouteController.cs
+++  smart-buy/Code/Smart Buy/SmartB.UI/Controllers/RouteController.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SmartB.UI.Models.EntityFramework;
 
 namespace SmartB.UI.Controllers
 {
     public class RouteController : Controller
     {
-        //
-        // GET: /Route/
+        private SmartBuyEntities context = new SmartBuyEntities();
 
         public ActionResult DefineRoute()
         {
-            return View();
+            var markets = context.Markets.Where(x => x.IsActive).ToList();
+            return View(markets);
         }
 
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }
