e4933a98f30a0d2b252d3fa352270ab5ab3fc9db
hoangdang.nt@gmail.com <hoangdang.nt@gmail.com@71cf5126-8dba-bb00-75b8-2b74b3ec1fb7>
2014-02-25 02:51:31 +0000

    add model ProductMostBuy

---
+++ trunk/ smart-buy/Code/Smart Buy/SmartB.UI/Models/ProductMostBuy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SmartB.UI.Models.EntityFramework;
+
+namespace SmartB.UI.Models
+{
+    public class ProductMostBuy
+    {
+        public Product Product { get; set; }
+        public int numberOfBuy { get; set; }
+    }
+}
