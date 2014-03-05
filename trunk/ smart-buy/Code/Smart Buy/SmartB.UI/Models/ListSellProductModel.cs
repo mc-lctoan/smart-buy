0d19d9810c91c7afb177ba2f535cecf745f01e3d
doanhoanhtriet@gmail.com <doanhoanhtriet@gmail.com@71cf5126-8dba-bb00-75b8-2b74b3ec1fb7>
2014-03-05 07:39:11 +0000

    - Update jquery. - Itegrate import function.

--- trunk/ smart-buy/Code/Smart Buy/SmartB.UI/Models/ListSellProductModel.cs
+++ trunk/ smart-buy/Code/Smart Buy/SmartB.UI/Models/ListSellProductModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using PagedList;
 
 namespace SmartB.UI.Models
 {
@@ -9,5 +10,6 @@ namespace SmartB.UI.Models
     {
         public List<SellProductModel> CorrectSellProducts { get; set; }
         public List<SellProductModel> InCorrectSellProducts { get; set; }
+        public IPagedList<SellProductModel> PagedCorrectProducts { get; set; }
     }
 }
