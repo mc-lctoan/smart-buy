bfbde290bbe36bed79d3989859afa3ad99220666
hoangdang.nt@gmail.com <hoangdang.nt@gmail.com@71cf5126-8dba-bb00-75b8-2b74b3ec1fb7>
2014-03-13 13:04:15 +0000

    - add api account - fix search in ProductApiController - add function get market

---
+++ trunk/ smart-buy/Code/Smart Buy/SmartB.UI/Controllers/AccountApiController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Security;
+using SmartB.UI.Models.EntityFramework;
+using SmartB.UI.Models;
+
+namespace SmartB.UI.Controllers
+{
+    public class AccountApiController : ApiController
+    {
+        private SmartBuyEntities db = new SmartBuyEntities();
+
+        [HttpGet]
+        public bool Login(String username, String password)
+        {
+            //JavaScriptSerializer ser = new JavaScriptSerializer();
+            //LoginModel parseJson = ser.Deserialize<LoginModel>(loginJson);
+
+            try
+            {               
+
+                bool HaveUser = db.Users.Any(u => u.Username == username && u.Password == password);
+
+                if (HaveUser)
+                {
+                    FormsAuthentication.SetAuthCookie(username, false);
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+    }
+}
