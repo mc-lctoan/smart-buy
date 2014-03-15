4692e9e38b5f69c7ed9bdb1f0d5b01ab0a345230
doanhoanhtriet@gmail.com <doanhoanhtriet@gmail.com@71cf5126-8dba-bb00-75b8-2b74b3ec1fb7>
2014-03-15 14:37:11 +0000

    - Suggest for 3 routes, done.

--- trunk/ smart-buy/Code/Smart Buy/SmartB.UI/Controllers/RouteController.cs
+++ trunk/ smart-buy/Code/Smart Buy/SmartB.UI/Controllers/RouteController.cs
@@ -10,6 +10,7 @@ using SmartB.UI.Models.EntityFramework;
 
 namespace SmartB.UI.Controllers
 {
+    [Authorize]
     public class RouteController : Controller
     {
         private SmartBuyEntities context = new SmartBuyEntities();
@@ -21,49 +22,116 @@ namespace SmartB.UI.Controllers
         }
 
         [HttpPost]
-        public ActionResult DefineRoute(DefineRouteModel model)
+        public ActionResult DefineRoute(AccountDetailModel model)
         {
-            if (model.Route == "")
-            {
-                ModelState.AddModelError("", "Phải chọn đường đi!");
-            }
-
             if (ModelState.IsValid)
             {
-                var user = context.Users.FirstOrDefault(x => x.Username == User.Identity.Name);
+                var user = context.Users
+                    .Include(x => x.Profile)
+                    .FirstOrDefault(x => x.Username == User.Identity.Name);
                 if (user != null)
                 {
-                    //user.DefinedRoute = model.Route;
-                    //user.MarketId = model.MarketId;
+                    var profile = new Profile
+                                      {
+                                          FirstRouteName = model.FirstRouteName,
+                                          FirstRoute = model.FirstRoute,
+                                          FirstMarkets = model.FirstMarkets,
+                                          SecondRouteName = model.SecondRouteName,
+                                          SecondRoute = model.SecondRoute,
+                                          SecondMarkets = model.SecondMarkets,
+                                          ThirdRouteName = model.ThirdRouteName,
+                                          ThirdRoute = model.ThirdRoute,
+                                          ThirdMarkets = model.ThirdMarkets
+                                      };
+                    user.Profile = profile;
+
                     context.SaveChanges();
                     TempData["DefineRoute"] = "Success";
-                    return RedirectToAction("DefineRoute");
+                    return RedirectToAction("AccountDetails", "Account");
                 }
                 ModelState.AddModelError("", "Username không tồn tại!");
             }
 
             // Something's wrong, redisplay the form
-            var markets = context.Markets.Where(x => x.IsActive).ToList();
-            return View(markets);
+            TempData["DefineRoute"] = "Fail";
+            return RedirectToAction("AccountDetails", "Account");
         }
 
-        [Authorize]
-        public ActionResult SuggestRoute(Cart cart)
+        public ActionResult SuggestRoute()
         {
             var model = new List<SuggestRouteModel>();
+            var routes = new List<SelectListItem>();
+            var user = context.Users
+                .Include(x => x.Profile)
+                .FirstOrDefault(x => x.Username == User.Identity.Name);
+            if (user != null)
+            {
+                if (user.Profile.FirstRoute != null)
+                {
+                    var item = new SelectListItem
+                                   {
+                                       Text = user.Profile.FirstRouteName,
+                                       Value = user.Profile.FirstRouteName
+                                   };
+                    routes.Add(item);
+                }
+                if (user.Profile.SecondRoute != null)
+                {
+                    var item = new SelectListItem
+                                   {
+                                       Text = user.Profile.SecondRouteName,
+                                       Value = user.Profile.SecondRouteName
+                                   };
+                    routes.Add(item);
+                }
+                if (user.Profile.ThirdRoute != null)
+                {
+                    var item = new SelectListItem
+                                   {
+                                       Text = user.Profile.ThirdRouteName,
+                                       Value = user.Profile.ThirdRouteName
+                                   };
+                    routes.Add(item);
+                }
+                ViewBag.Routes = routes;
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult SuggestRoute(Cart cart, string Routes)
+        {
+            var model = new List<SuggestRouteModel>();
+            var routes = new List<SelectListItem>();
 
             var cartProducts = cart.Lines.Select(x => x.Product.Product).ToList();
 
-            var user = context.Users.FirstOrDefault(x => x.Username == User.Identity.Name);
+            var user = context.Users
+                .Include(x => x.Profile)
+                .FirstOrDefault(x => x.Username == User.Identity.Name);
             if (user != null)
             {
-                // TODO: haven't defined? Redirect to define route, show error
-                // Get route
-                //ViewBag.Route = user.DefinedRoute;
+                string chosenRoute = "";
+                string[] ids = null;
 
-                // Get nearby markets id
-                //string[] ids = user.MarketId.Split(',');
-                string[] ids = new string[10];
+                if (user.Profile.FirstRouteName == Routes)
+                {
+                    chosenRoute = user.Profile.FirstRoute;
+                    ids = user.Profile.FirstMarkets.Split(',');
+                } 
+                else if (user.Profile.SecondRouteName == Routes)
+                {
+                    chosenRoute = user.Profile.SecondRoute;
+                    ids = user.Profile.SecondMarkets.Split(',');
+                }
+                else if (user.Profile.ThirdRouteName == Routes)
+                {
+                    chosenRoute = user.Profile.ThirdRoute;
+                    ids = user.Profile.ThirdMarkets.Split(',');
+                }
+                
+                // Get route
+                ViewBag.Route = chosenRoute;
 
                 // Construct a market list
                 var markets = new List<Market>();
@@ -93,6 +161,36 @@ namespace SmartB.UI.Controllers
 
                 var route = new SuggestRouteHelper(products, markets);
                 model = route.Suggest();
+
+                // Construct the dropdownlist
+                if (user.Profile.FirstRoute != null)
+                {
+                    var item = new SelectListItem
+                    {
+                        Text = user.Profile.FirstRouteName,
+                        Value = user.Profile.FirstRouteName
+                    };
+                    routes.Add(item);
+                }
+                if (user.Profile.SecondRoute != null)
+                {
+                    var item = new SelectListItem
+                    {
+                        Text = user.Profile.SecondRouteName,
+                        Value = user.Profile.SecondRouteName
+                    };
+                    routes.Add(item);
+                }
+                if (user.Profile.ThirdRoute != null)
+                {
+                    var item = new SelectListItem
+                    {
+                        Text = user.Profile.ThirdRouteName,
+                        Value = user.Profile.ThirdRouteName
+                    };
+                    routes.Add(item);
+                }
+                ViewBag.Routes = routes;
             }
 
             return View(model);
---
+++ trunk/ smart-buy/Code/Smart Buy/SmartB.UI/Models/AccountDetailModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using SmartB.UI.Models.EntityFramework;
+
+namespace SmartB.UI.Models
+{
+    public class AccountDetailModel
+    {
+        [Display(Name = "Tên đăng nhập")]
+        public string Username { get; set; }
+
+        [Display(Name = "Mật khẩu")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Mật khẩu mới")]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "{0} phải có ít nhất {2} ký tự.", MinimumLength = 6)]
+        public string NewPassword { get; set; }
+
+        public List<Market> Markets { get; set; }
+
+        public string FirstRoute { get; set; }
+        public string FirstMarkets { get; set; }
+        public string FirstRouteName { get; set; }
+        public string SecondRoute { get; set; }
+        public string SecondMarkets { get; set; }
+        public string SecondRouteName { get; set; }
+        public string ThirdRoute { get; set; }
+        public string ThirdMarkets { get; set; }
+        public string ThirdRouteName { get; set; }
+    }
+}
