f127392fb639ddd1807bce932208c5bb9e649167
hoangdang.nt@gmail.com <hoangdang.nt@gmail.com@71cf5126-8dba-bb00-75b8-2b74b3ec1fb7>
2014-03-17 04:26:45 +0000

    add more function in api

---
+++  smart-buy/Code/Smart Buy/SmartB.UI/Models/HistoryDetailMobileModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SmartB.UI.Areas.Admin.Models;
+using SmartB.UI.Models.EntityFramework;
+
+namespace SmartB.UI.Models
+{
+    public class HistoryDetailMobileModel
+    {
+        public int Id { get; set; }
+        public int HistoryId { get; set; }
+        public string ProductName { get; set; }
+        public int MinPrice { get; set; }
+        public int MaxPrice { get; set; }
+        public int MinPriceToday { get; set; }
+        public int MaxPriceToday { get; set; }
+    }
+}
