672a7489a960242615bf50367f1cef1d03502e90
doanhoanhtriet@gmail.com <doanhoanhtriet@gmail.com@71cf5126-8dba-bb00-75b8-2b74b3ec1fb7>
2014-03-30 09:01:33 +0000

    - Fix bug.

---  smart-buy/Code/Smart Buy/SmartB.UI/Helper/MathHelper.cs
+++  smart-buy/Code/Smart Buy/SmartB.UI/Helper/MathHelper.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
+using System.Xml.Linq;
 
 namespace SmartB.UI.Helper
 {
@@ -29,6 +32,50 @@ namespace SmartB.UI.Helper
         private double DegreeToRad(double deg)
         {
             return deg * (Math.PI / 180);
+        }
+
+        public double TravelDistance(double lat1, double lng1, double lat2, double lng2)
+        {
+            const string url = "https://maps.googleapis.com/maps/api/distancematrix/xml?";
+            const string key = "AIzaSyAmQi6XuUHARe_gzLWFpKWzZSu34ZaWv1Q";
+            const string sensor = "false";
+            string origins = lat1 + "," + lng1;
+            string destinations = lat2 + "," + lng2;
+
+            string fullPath = url + "key=" + key + "&origins=" + origins + "&destinations=" + destinations + "&sensor=" + sensor;
+
+            var client = new WebClient();
+            Stream stream = client.OpenRead(fullPath);
+            XDocument doc = XDocument.Load(stream);
+            try
+            {
+                var root = doc.Element("DistanceMatrixResponse");
+                var status = root.Element("status").Value;
+                if (status != "OK")
+                {
+                    return -1;
+                }
+
+                var distance = root.Element("row")
+                    .Element("element")
+                    .Element("distance")
+                    .Element("value");
+                if (distance != null)
+                {
+                    double result;
+                    bool flag = Double.TryParse(distance.Value, out result);
+                    if (flag)
+                    {
+                        return result / 1000;
+                    }
+                }
+            }
+            catch (NullReferenceException)
+            {
+                return 0;
+            }
+            
+            return 0;
         }
     }
 }
