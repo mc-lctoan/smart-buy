0d4ee281576ebc9f7805261669c2a8b39ed1dd95
doanhoanhtriet@gmail.com <doanhoanhtriet@gmail.com@71cf5126-8dba-bb00-75b8-2b74b3ec1fb7>
2014-03-31 17:07:52 +0000

    - Replace Fluent Scheduler with Quartz.

---
+++  smart-buy/Code/Smart Buy/SmartB.UI/App_Start/BackgroundConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quartz;
+using Quartz.Core;
+using Quartz.Impl;
+using SmartB.UI.Infrastructure;
+
+namespace SmartB.UI.App_Start
+{
+    public static class BackgroundConfig
+    {
+        private static IScheduler scheduler;
+
+        public static void StartScheduler()
+        {
+            scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+        }
+
+        public static void ScheduleParser()
+        {
+            IJobDetail job = JobBuilder.Create<ParseJob>()
+                .WithIdentity("Parser", "Job")
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("ParserTrigger", "Trigger")
+                .WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(4, 0))
+                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+
+        public static void RescheduleParser(int hour, int minute)
+        {
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("ParserTrigger", "Trigger")
+                .WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(hour, minute))
+                .Build();
+            //scheduler.RescheduleJob(parserTrigger.Key, trigger);
+        }
+    }
+}
---
+++  smart-buy/Code/Smart Buy/SmartB.UI/Infrastructure/ParseJob.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quartz;
+using SmartB.UI.Areas.Admin.Helper;
+
+namespace SmartB.UI.Infrastructure
+{
+    public class ParseJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            ParseHelper.ParseData();
+        }
+    }
+}
