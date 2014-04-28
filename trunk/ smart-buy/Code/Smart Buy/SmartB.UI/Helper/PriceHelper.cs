b5e187858e055127f5e9593c6ab20eef9fb9c63b
doanhoanhtriet@gmail.com <doanhoanhtriet@gmail.com@71cf5126-8dba-bb00-75b8-2b74b3ec1fb7>
2014-04-28 04:59:39 +0000

    - Calculate price range.

---
+++ trunk/ smart-buy/Code/Smart Buy/SmartB.UI/Helper/PriceHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SmartB.UI.Models.EntityFramework;
+
+namespace SmartB.UI.Helper
+{
+    public class PriceHelper
+    {
+        public void CalculatePriceRange(int productId)
+        {
+            using (var context = new SmartBuyEntities())
+            {
+                // Calculate min, max price for today
+                var priceList = context.SellProducts
+                    .Where(x => x.ProductId == productId && x.LastUpdatedTime.Date == DateTime.Today)
+                    .ToList();
+                var minPrice = priceList.Min(x => x.SellPrice);
+                var maxPrice = priceList.Max(x => x.SellPrice);
+
+                var attribute = new ProductAttribute
+                                    {
+                                        ProductId = productId,
+                                        MinPrice = minPrice,
+                                        MaxPrice = maxPrice,
+                                        LastUpdatedTime = DateTime.Now
+                                    };
+                context.ProductAttributes.Add(attribute);
+                context.SaveChanges();
+            }
+        }
+    }
+}
